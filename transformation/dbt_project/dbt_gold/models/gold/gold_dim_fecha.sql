3fc7ff6f4a3a38dc0ee98ab90fd24ba98a7b6e15
JuanGonzalez928 <juandediosgn7@gmail.com>
2026-05-25 04:41:32 -0500

    feat: Gold layer completa con Star Schema y 30 tests PASS

--- transformation/dbt_project/dbt_gold/models/gold/gold_dim_fecha.sql
+++ transformation/dbt_project/dbt_gold/models/gold/gold_dim_fecha.sql
@@ -5,12 +5,12 @@
 
 SELECT DISTINCT
     CAST(DATE_FORMAT(event_datetime, 'yyyyMMdd') AS INT) AS fecha_key,
-    CAST(event_datetime AS DATE)                          AS fecha,
-    review_year                                           AS anio,
-    review_month                                          AS mes,
-    review_day                                            AS dia,
-    day_of_week                                           AS dia_semana,
-    QUARTER(event_datetime)                               AS trimestre,
-    CASE WHEN day_of_week IN (1, 7) THEN true ELSE false END AS es_fin_de_semana
-FROM {{ ref('silver_reviews') }}
+    CAST(event_datetime AS DATE) AS fecha,
+    YEAR(event_datetime) AS anio,
+    MONTH(event_datetime) AS mes,
+    DAY(event_datetime) AS dia,
+    DAYOFWEEK(event_datetime) AS dia_semana,
+    QUARTER(event_datetime) AS trimestre,
+    CASE WHEN DAYOFWEEK(event_datetime) IN (1,7) THEN true ELSE false END AS es_fin_de_semana
+FROM {{ source('silver', 'reviews_clean') }}
 WHERE event_datetime IS NOT NULL
